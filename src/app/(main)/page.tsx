f50d25b81f097b2c8e9b0072d6b2afbd11c03129
sina <sinaaamlk@gmail.com>
2024-11-02 10:50:47 +0330

    loading posts server-side

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,21 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
+import prisma from "@/lib/prisma";
+import Post from "./Post";
+import { postDataInclude } from "@/lib/types";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await prisma.post.findMany({
+    include: postDataInclude,
+    orderBy: { createdAt: "desc" },
+  });
+
   return (
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
       </div>
     </main>
   );
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,22 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { formatDate, formatDistanceToNowStrict } from "date-fns";
+import { fromJSON } from "postcss";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatRelativeDate(form: Date) {
+  const currentDate = new Date();
+
+  if (currentDate.getTime() - form.getTime() < 24 * 60 * 60 * 1000) {
+    return formatDistanceToNowStrict(form, { addSuffix: true });
+  }else{
+    if(currentDate.getFullYear() === form.getFullYear()){
+      return formatDate(form,"MMM d")
+    }else{
+      return formatDate(form,"MMM d, yyyy")
+    }
+  }
 }
